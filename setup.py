152b5db9ee966b9c15a51a1c869df0c9d5244d95
Vatsal Mehta <vatsalbmehta3@gmail.com>
2024-12-09 04:50:57 -0500

    updated gitignore to include food.com recipes and added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mealmatch",
+    version="0.1",
+    description="MealMatch CS4973 Project",
+    url="https://github.com/VatsalMehta27/mealmatch",
+    packages=find_packages(),
+    python_requires=">=3.11",
+    install_requires=[
+        "numpy",
+        "transformers",
+        "beautifulsoup4",
+        "chromadb",
+        "openai",
+        "scikit-learn",
+        "matplotlib",
+        "python-dotenv",
+        "gradio",
+        "tqdm",
+        "torch",
+        "recipe-scrapers",
+        "rapidfuzz",
+        "requests",
+    ],
+)
